a45522dbfa8f55aa4a62b52bbe3c0bd276ae34ba
Aleksei <katakuxiko@gmail.com>
2022-04-07 17:06:12 +0600

    Vse robit

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express');
 const router = new Router();
+const deviceController = require('../controllers/deviceController');
 
 
-router.post('/',);
-router.post('/',);
-router.get('/:id',);
+
+router.post('/',deviceController.create);
+router.get('/',deviceController.getAll);
+router.get('/:id',deviceController.getOne);
 
 
 module.exports = router;
